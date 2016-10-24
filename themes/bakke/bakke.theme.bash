2d600f95968e4157260f3be71dc9b3ad671a0ed0
Todd Trimmer <todd.trimmer@gmail.com>
2016-10-24 15:05:43 -0500

    need dem sq brackets!

--- themes/bakke/bakke.theme.bash
+++ themes/bakke/bakke.theme.bash
@@ -6,11 +6,13 @@
 # Comment
 SCM_THEME_PROMPT_DIRTY=" ${red}✗"
 SCM_THEME_PROMPT_CLEAN=" ${bold_green}✓"
-SCM_THEME_PROMPT_SUFFIX="${normal}|"
+SCM_THEME_PROMPT_PREFIX=" ["
+SCM_THEME_PROMPT_SUFFIX="${normal}"
 
 GIT_THEME_PROMPT_DIRTY=" ${red}✗"
 GIT_THEME_PROMPT_CLEAN=" ${bold_green}✓"
-GIT_THEME_PROMPT_SUFFIX="${normal}|"
+GIT_THEME_PROMPT_PREFIX=" ["
+GIT_THEME_PROMPT_SUFFIX="${normal}"
 
 function prompt_command() {
     #PS1="${bold_cyan}$(scm_char)${green}$(scm_prompt_info)${purple}$(ruby_version_prompt) ${yellow}\h ${reset_color}in ${green}\w ${reset_color}\n${green}→${reset_color} "
@@ -18,7 +20,7 @@ function prompt_command() {
     #PS1="\n${cyan}\h: ${reset_color} ${yellow}\w\n${red}$(scm_char)${red}$(scm_prompt_info) ${green}→${reset_color} "
     #PS1="[\[\e[35m\]\t \[\e[36m\]\h \[\e[33m\]\W\[\e[0m\]]\$"
     #PS1="\n${cyan}\h: ${reset_color} ${yellow}\w ${green}$(scm_prompt_info)\n${reset_color}→ "
-    PS1="${purple}\t ${cyan}\h ${yellow}\W${normal}$(scm_prompt_info) \$ "
+    PS1="${purple}\t ${cyan}\h ${yellow}\W${normal}$(scm_prompt_info)]\$ "
 }
 
 safe_append_prompt_command prompt_command
